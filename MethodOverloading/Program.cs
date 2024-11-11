82b805392cb44dc2425be35bea16de26005eac7b
PamelaNicks <pamelanicks@yahoo.com>
2024-11-10 20:44:30 -0600

    initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,52 @@
 {
     public class Program
     {
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static string Add(int numOne, int numTwo, bool isCurrency)
+        {
+            var sum = numOne + numTwo;
+
+            if (isCurrency && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isCurrency && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }           
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var x = 7;
+            var y = 12;
+            
+            var answer = Add(x, y);
+
+            var a = 7.0m;
+            var b = 12.0m;
+
+            var decimalAnswer = Add(a,b);
+
+            var thirdAnswer = Add(0, 0, true);
+
+            Console.WriteLine($"int add: {answer} decimal add: {decimalAnswer}");
+            Console.WriteLine(Add(x, y, true));
         }
     }
 }
